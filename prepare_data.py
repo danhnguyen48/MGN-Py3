2063369346f1ff0a478439e2608c264d5aed269f
Danh Nguyen <danh.nghuu@gmail.com>
2021-01-22 20:05:29 +0700

    update saving feature

--- prepare_data.py
+++ prepare_data.py
@@ -10,11 +10,9 @@ data_dict = {}
 
 def load_images_from_folder(folder):
     image_ind = 0
-    f = open(MAP_SEG, 'r')
-    while True:
-        line = f.readline()
-        if not line:
-            break
+    with open(MAP_SEG, 'r') as f:
+      alist = f.read().splitlines()
+      for line in alist:
         img = cv2.imread(os.path.join(folder, line))
         encode_image_to_dict(img, image_ind)
         image_ind+=1
@@ -22,30 +20,30 @@ def load_images_from_folder(folder):
 
 def encode_image_to_dict(image, ind):
     im = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
-    data_dict['image_' + ind] = [im]
+    data_dict['image_' + str(ind)] = [im]
 
 def load_joints_from_folder(folder):
     joint_ind = 0
     f = open(MAP_JOINTS, 'r')
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        joints = json.load(os.path.join(folder, line))
-        data = np.asarray(data['people'][0]['pose_keypoints_2d'])
+    with open(MAP_JOINTS, 'r') as f:
+      alist = f.read().splitlines()
+      for line in alist:
+        with open(os.path.join(folder, line)) as f:
+          joints = json.load(f)
+        data = np.asarray(joints['people'][0]['pose_keypoints_2d'])
         data = data.reshape(25,3)
         encode_joints_to_dict(data, joint_ind)
         joint_ind+=1
 
 def encode_joints_to_dict(joints, ind):
-    data_dict['J_2d_' + ind] = [joints]
+    data_dict['J_2d_' + str(ind)] = [joints]
 
 def getVertexLabel():
     TEMPLATE = pickle.load(
         open('assets/allTemplate_withBoundaries_symm.pkl', 'rb'), encoding="latin1")
     data_dict["vertexlabel"]=np.zeros((1,27554,1))   #batch_size x vertices x 1
-    data_dict["vertexlabel"][np.where(TEMPLATE['Pants'][1])[0]] = 1
-    data_dict["vertexlabel"][np.where(TEMPLATE['TShirtNoCoat'][1])[0]] = 5
+    data_dict["vertexlabel"][0][np.where(TEMPLATE['Pants'][1])[0]] = 1
+    data_dict["vertexlabel"][0][np.where(TEMPLATE['TShirtNoCoat'][1])[0]] = 5
 
 def savePickleFile():
     outfile = open('assets/inp_data.pkl', 'wb')
